f1a6305185d11aff7e526df6689dbf55dfab0410
tonistorres <sisvenda2011@gmail.com>
2021-08-25 19:22:13 -0300

    variaveis e tipos primitivos js basico

---
+++ fundamentos/bloco-04-introducao-java-script-e-logica-programacao/variaveis.js
@@ -0,0 +1,23 @@
+/*
+
+1-Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+2-Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+3-Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+4-Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+*/
+
+const myName = "Tonis Alberto Torres Ferreira";
+const birthCity = "Alto Alegre do Maranhão";
+let birthYear = 1979;
+console.log('Meu Nome e: ' + myName + ' Moro em: ' + birthCity + ' Ano de Nascimento é :' + birthYear);
+
+//5-Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+birthYear=2030;
+console.log(birthYear);
+
+//Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro?
+
+/*Bem na minha concepção não podemos alterar ou atribuir um valor a uma constante que o caso aqui apresentado 
+,pois, o próṕrio nome ja deixa explícito essa não posssibilidade de manipulação, ou seja, sua existencia já 
+é uma correção para não haja bugs em seu código é uma segurança criada para trabalharmos com valores fixos ao 
+logo de de todo scopo do programa*/ 
---
+++ fundamentos/bloco-04-introducao-java-script-tipos-primitivos/tiposPrimitivos.js
@@ -0,0 +1,38 @@
+
+/*
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientAge); // indefinido, pois, este valor não foi definido no scopo do programa 
+
+
+*/
+
+
+const base=5;
+let height=8;
+//     5cm
+//  ------------------ RETANGULO
+//  |                |
+//  |                | 
+//  |                | 8cm 
+//  |                |  
+//  -------------------
+//sabendo que o perímetro é calculado a partir da soma de todos os lados do retângulo podemos presumir 
+
+let perimetro=(2*base)+(2*height);
+console.log('Perímetro: '+ perimetro +'cm');
+
+//Sabendo que a Área de um Retangulo é base * altura  podes fazer 
+
+let areaRetangulo=base*height;
+console.log('Área do Retângulo é :' + areaRetangulo+ 'cm2');
+
+
+
+
