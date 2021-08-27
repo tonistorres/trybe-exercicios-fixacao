790990ee147e144708d57f057cfa5bd50e53888a
tonistorres <sisvenda2011@gmail.com>
2021-08-27 11:26:24 -0300

    array-for-somatorio

---
+++ fundamentos/bloco-04-introducao-javascript-array-somatorio-for/arrayFruits.js
@@ -0,0 +1,14 @@
+/*Utilizando o array abaixo, percorra-o somando todos os valores.
+ Caso o valor final seja maior que 15, imprima-o. Caso seja igual 
+ ou menor que 15, imprima a mensagem: "valor menor que 16":*/
+
+let fruits = [3, 4, 10, 1, 12];
+let summation = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+
+
+    summation += fruits[index];
+
+}
+console.log("Somatório : ", summation);
